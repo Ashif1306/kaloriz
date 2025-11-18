343297e4318b27e7230e833f6002a7a6fd52a9a1
Ashif1306 <devashif.md@gmail.com>
2025-11-18 15:57:55 +0800

    update

--- kaloriz/kaloriz/urls.py
+++ kaloriz/kaloriz/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls")),
@@ -12,4 +13,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
