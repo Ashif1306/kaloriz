2a8959090049cda43c9f4e6270104d5331d3b1d2
Ashif1306 <devashif.md@gmail.com>
2025-11-18 16:05:36 +0800

    update

--- kaloriz/kaloriz/urls.py
+++ kaloriz/kaloriz/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path("payment/", include("payment.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# SELALU tambahkan static & media (tidak pakai if settings.DEBUG)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
